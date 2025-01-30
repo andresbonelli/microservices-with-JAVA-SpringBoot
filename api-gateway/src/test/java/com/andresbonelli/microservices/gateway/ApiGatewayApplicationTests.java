e8cadf2a23a99758a862cb5ada56c1e756c8ac5a
andresbonelli <andresbonellipiano@gmail.com>
2025-01-30 10:56:42 -0300

    API Gateway implementation

---
+++ api-gateway/src/test/java/com/andresbonelli/microservices/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresbonelli.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
